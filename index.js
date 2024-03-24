73f649e87da4bf306782e5ecbeb16a003845e65e
Luther Isaboke <lusaboke@gmail.com>
2024-03-24 13:45:08 +0300

    arithmeticlab

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
 // Write your code here
+// creating var multiply that should output 62
+let num1 = 31;
+let num2 = 2;
+const multiply = num1 * num2 ;
+
+//creating var random that generates random number > 0
+
+const random = Math.round (Math.random () * 10)+1;
+
+//to create an eqtn that calculates a remainder equal to 4
+
+let num3 =9;
+let num4 =5;
+let mod = num3 % num4;
+
+//to find the highest number in a set
+
+let max = Math.max(12,13,14,16,20);
